f585b510681243b3a582e9f7978cad922d7423fe
Vova Pesochniy <vpesochniy@yandex.kz>
2024-10-24 02:02:12 +0300

    fix(UserService): fix path to MOCK_DATA.json to be able to run app as jar file The previous implementation used an absolute path to the MOCK_DATA.json file, which is not suitable for running the application as a jar file. The updated implementation uses ClassPathResource to load the file from the classpath, which allows the application to be run as a jar file.

--- src/main/java/ru/t1/demo/kafka/service/UserService.java
+++ src/main/java/ru/t1/demo/kafka/service/UserService.java
@@ -1,12 +1,12 @@
 package ru.t1.demo.kafka.service;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -39,12 +39,14 @@ public class UserService {
     }
 
     private List<UserDto> parseUsersFromJson() {
+
+        ClassPathResource resource = new ClassPathResource("MOCK_DATA.json");
         ObjectMapper objectMapper = new ObjectMapper();
 
         UserDto[] parsedUsers;
 
         try {
-            parsedUsers = objectMapper.readValue(new File("/home/vpesochniy/Desktop/ru-t1-demo-kafka/src/main/resources/MOCK_DATA.json"), UserDto[].class);
+            parsedUsers = objectMapper.readValue(resource.getFile(), UserDto[].class);
         } catch (IOException ex) {
             ex.printStackTrace();
             return Collections.emptyList();
